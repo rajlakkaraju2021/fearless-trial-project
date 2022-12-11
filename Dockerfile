aeb7d6be2809c974e85c956a59d18121accda72e
rajlakkaraju2021 <rajlakkaraju2021@gmail.com>
2022-12-10 19:18:45 -0600

    Added Local DB to Docker

--- Dockerfile
+++ Dockerfile
@@ -13,5 +13,8 @@ COPY . .
 # expose the app port
 EXPOSE 3000
 
+# create a database directory
+RUN mkdir -p /usr/src/app/database
+
 # start the app
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 // create an express app
 const app = express();
@@ -9,21 +10,51 @@ app.use(express.json());
 // define the port to run the app on
 const port = 3000;
 
-// create an array to hold the items (Persist in-memory)
+// create an array to hold the items
 let items = [];
 
 // create a route to handle requests to the /items endpoint
 app.route('/items')
   // handle requests to retrieve the current items
-  .get((req, res) => res.json(items))
+  .get((req, res) => 
+    {
+      // load the items from the database
+      fs.readFile('database/items.json', (err, data) => {
+        if (err) {
+          res.json([])
+        } else {
+          items = JSON.parse(data);
+          res.json(items)
+        }
+      });
+    }
+  )
   // handle requests to set the list of items
   .post((req, res) => {
-    items = req.body;
-    res.json(items);
+    fs.readFile('database/items.json', (err, data) => {
+        if(err){
+          items = []
+        }
+        else{
+          items = JSON.parse(data)
+        }
+        items.push(req.body)
+        fs.writeFile('database/items.json', JSON.stringify(items), (err) => {
+          if (err) {
+            console.log('Error saving items to database');
+          }
+        });
+        res.json(items)
+      })
   })
   // handle requests to delete all of the items
   .delete((req, res) => {
     items = [];
+    fs.writeFile('database/items.json', JSON.stringify(items), (err) => {
+      if (err) {
+        console.log('Error saving items to database');
+      }
+    });
     res.send('All items have been deleted');
   });
 
